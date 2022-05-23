0e8ae98e595d0a8ee86f80b46dd6332bce487ddd
Zulfiyya Huseynova <tu7kswf4d@code.edu.az>
2022-05-23 10:34:43 +0400

    task added

---
+++ karma/karma/karma/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace karma.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ karma/karma/karma/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace karma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Category()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
